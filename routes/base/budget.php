4aafb9f274b8de329aee5d97bade22926669f8c9
Miriam Dominguez <miriam.dominguez@metimur.mx>
2024-11-30 17:14:46 -0600

    dashboard: add new routes

---
+++ routes/base/budget.php
@@ -0,0 +1,17 @@
+
+<?php
+
+/**
+ * This file contains the routes for the tools.
+ */
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BudgetController;
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::middleware(['can:ver_cotizaciones'])->group(function () {
+        Route::get('/cotizaciones/home', [BudgetController::class, 'index'])->name('cotizaciones.home');
+        Route::post('/cotizaciones/create', [BudgetController::class, 'store'])->name('cotizaciones.store');
+        Route::delete('/cotizaciones/delete/{id}', [BudgetController::class, 'destroy'])->name('cotizaciones.destroy');
+    });
+});

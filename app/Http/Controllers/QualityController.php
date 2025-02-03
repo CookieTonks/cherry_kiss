8d593cc97c996388f78a175f711cee89703e480c
Miriam Dominguez <miriam.dominguez@metimur.mx>
2025-02-03 14:41:12 -0600

    Quality: new module

---
+++ app/Http/Controllers/QualityController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class QualityController extends Controller
+{
+    //
+
+    public function Home()
+    {
+        return view('vistas.quality.home');
+    }
+}
---
+++ routes/base/quality.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * This file contains the routes for the tools.
+ */
+
+use App\Http\Controllers\QualityController;
+use Illuminate\Support\Facades\Route;
+
+Route::group(['middleware' => ['auth']], function () {
+    // Route::middleware(['can:ver_usuarios'])->group(function () {
+    Route::get('/quality/home', [QualityController::class, 'Home'])->name('quality.home');
+
+
+
+    // });
+});

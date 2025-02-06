8b02cc5bf9b2954f5cd62bf609aebf59ec0f7bb5
Miriam Dominguez <miriam.dominguez@metimur.mx>
2025-02-06 15:58:34 -0600

    Budget: filtro

--- routes/base/quality.php
+++ routes/base/quality.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Route;
 Route::group(['middleware' => ['auth']], function () {
     // Route::middleware(['can:ver_usuarios'])->group(function () {
     Route::get('/quality/home', [QualityController::class, 'Home'])->name('qualitys.home');
-    Route::post('/quality/ot/{id}/liberacion', [QualityController::class, 'liberacion'])->name('quality.ot.liberacion');
+    Route::post('/quality/ot/{id}/liberacion', [QualityController::class, 'liberacion'])->name('qualitys.ot.liberacion');
 
 
 

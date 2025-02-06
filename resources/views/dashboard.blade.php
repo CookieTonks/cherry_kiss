abbf2617041ee573fa163ddd4142e874dbd8440a
Miriam Dominguez <miriam.dominguez@metimur.mx>
2025-02-05 20:17:25 -0600

    Fix: normal

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -98,7 +98,6 @@
                 <div class="col-12 col-sm-4">
                     <div class="card shadow rounded h-100 d-flex align-items-center justify-content-center">
                         <div class="card-body text-center">
-                            <a href="{{ route('shipping.home') }}" class="text-decoration-none text-dark fw-bold fs-5">
                                 Embarques
                             </a>
                         </div>
@@ -108,7 +107,6 @@
                 <div class="col-12 col-sm-4">
                     <div class="card shadow rounded h-100 d-flex align-items-center justify-content-center">
                         <div class="card-body text-center">
-                            <a href="{{ route('budgets.index') }}" class="text-decoration-none text-dark fw-bold fs-5">
                                 Facturacion
                             </a>
                         </div>
@@ -118,7 +116,6 @@
                 <div class="col-12 col-sm-4">
                     <div class="card shadow rounded h-100 d-flex align-items-center justify-content-center">
                         <div class="card-body text-center">
-                            <a href="{{ route('budgets.index') }}" class="text-decoration-none text-dark fw-bold fs-5">
                                 Administracion
                             </a>
                         </div>
